b4585b4a8c6aaf538883c88b58f01c590208396b
poobesh <poobesh1999@gmail.com>
2020-05-28 23:33:32 +0530

    Updated Data Design and Spring Boot Initial Setup

---
+++ aumanagement/src/test/java/com/accolite/aumanagement/AuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accolite.aumanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
